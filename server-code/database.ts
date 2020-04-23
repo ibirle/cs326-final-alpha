12d6b5a0ca2e7d2b1a74b2937ff13b3e527c1ce8
Alain-Duplan <60901603+Alain-Duplan@users.noreply.github.com>
2020-04-23 17:13:15 -0400

    added awaits

--- server-code/database.ts
+++ server-code/database.ts
@@ -84,4 +84,13 @@ export class Database {
         return {};
     }
 
+    public async getAccount(key: string) : Promise<Object> {
+        this.client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+            if (err) throw err;
+            for (let row of res.rows) {
+              console.log(JSON.stringify(row));
+            }
+        });
+        return {};
+    }
 }
