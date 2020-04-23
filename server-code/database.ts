9c0c8aa7eb7ebc82e36d87dcbff809aa06292481
alexmwinter <awinter@umass.edu>
2020-04-23 00:18:11 -0400

    database heroku stuff

--- server-code/database.ts
+++ server-code/database.ts
@@ -1,6 +1,15 @@
+const { Client } = require('pg');
+
 export class Database {
 
+    private client;
+
     constructor() {
+
+        this.client = new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl: true,
+        });
 
         (async () => {
             await this.client.connect().catch(err => { console.log(err); });
@@ -8,7 +17,16 @@ export class Database {
     }
 
     public async getAllCurrentChallenges(key: string, value: string) : Promise<Object> {
+        this.client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+            if (err) throw err;
+            for (let row of res.rows) {
+              console.log(JSON.stringify(row));
+            }
+        });
         return {};
     }
+    
+
+
 
 }
