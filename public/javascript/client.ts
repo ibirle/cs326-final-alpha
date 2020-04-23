fe28da914e48e5ed24273a88f9e504b27c1cf683
alexmwinter <awinter@umass.edu>
2020-04-23 16:57:20 -0400

    Starting to use browserify

---
+++ public/javascript/client.ts
@@ -0,0 +1,6 @@
+import { Account } from './Account';
+import { challenge_page } from './challenge_page';
+import { forgot } from './forgot';
+import { index } from './index';
+import { login } from './login';
+import { newUser } from './newUser';
